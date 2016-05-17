a942dcf99aa0e5fadca7a4718c212dd6fa64d816
Alberto Buritica <alberto.buritica@outlook.es>
2016-05-17 13:17:42 +0200

    Add a base kivy panel settings
    The app needs a panel setting to customize the kickstart file. It has added
    three new functions build_config, build_setting in the main file. Also has
    been create a file, called settings.json. Now the button configure brings us
    to a panel setting when we can see a few options.

--- kickoff/main.py
+++ kickoff/main.py
@@ -5,12 +5,14 @@ This file contain the code of the main windows.
 """
 
 
-from validation import Validate
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 from kivy.properties import NumericProperty, StringProperty
+from kivy.uix.settings import SettingsWithTabbedPanel
+from kivy.uix.settings import Settings
 from kivy.clock import Clock
+from validation import Validate
 
 
 Builder.load_file('main.kv')
@@ -39,11 +41,12 @@ mkdir /etc/post
         pass
 
     def validate(self):
-        """Validating."""
+        """Calling the function update_progressbar."""
         self.progress = 0
         Clock.schedule_interval(self.update_progressbar, 1 / 60)
 
     def update_progressbar(self, dt):
+        """Updating progress bar and activating validation."""
         if self.progress != 100 and self.progress != -1:
             self.progress += 1
             self.status = 'Validating: {}%'.format(int(self.progress))
@@ -52,13 +55,38 @@ mkdir /etc/post
             self.progress = -1
             self.status = check
 
+
+
+
+
 class MainWindowApp(App):
     """Class MainWindowApp."""
 
+    use_kivy_settings = True    
+
     def build(self):
-        """Build function."""
+        """Build the main window and set the title."""
+        self.settings_cls = KickoffSettings
         self.title = "Kickoff | Kickstart file configurator"
         return MainWindow()
+
+    def build_config(self, config):
+        """Initial settings."""
+        config.setdefaults('User settings', 
+                           {
+                            'root': 1,
+                            'username': 'aburitica'
+                           })
+
+    def build_settings(self, settings):
+        """Using .json file and load."""
+        with open("settings.json") as s_json:
+            settings.add_json_panel('User settings', self.config,
+                                    data=s_json.read())
+
+class KickoffSettings(Settings):
+    pass
+
 
 if __name__ == "__main__":
     MainWindowApp().run()
