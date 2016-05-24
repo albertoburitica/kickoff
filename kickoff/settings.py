f530621413145a8fc00b4e149e9077436967849d
Alberto Buritica <alberto.buritica@outlook.es>
2016-05-24 12:09:07 +0200

    Created setting.py file
    It has been separated settings control from main.py in order to have
    everything more easy to manage.

---
+++ kickoff/settings.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""Setting.
+
+This file contains the logic of settings screens.
+"""
+
+
+from kivy.lang import Builder
+from kivy.properties import BooleanProperty, StringProperty
+from kivy.uix.popup import Popup
+from kivy.uix.screenmanager import Screen
+from user import User
+
+
+Builder.load_file('settings.kv')
+
+
+class InfoPopup(Popup):
+    """Popup."""
+
+    info = StringProperty('')
+
+    def set_info(self, message):
+        """Write a message into Popup window."""
+        self.info = message
+
+    def on_press_dismiss(self, *args):
+        """On press."""
+        self.dismiss()
+        return False
+
+
+class Settings(Screen):
+    """Settings screen."""
+
+    # Properties interacting with the user settings form.
+    root_pass_state = root_pass_conf_state = BooleanProperty(True)
+
+    def active_root(self, instance, value):
+        """Print."""
+        self.active = value
+        if self.active is True:
+            self.root_pass_state = False
+            self.ids.pr1.focus = True
+        elif self.active is False:
+            self.root_pass_conf_state = True
+            self.ids.pr1.text = ''
+            self.ids.pr2.text = ''
+
+    def on_focus(self, instance, value):
+        """Check root password."""
+        if not value:
+            if self.ids.pr1.text != self.ids.pr2.text:
+                popup = InfoPopup()
+                popup.set_info('Root passwords do not match')
+                popup.open()
+
+    def validate_forms(self):
+        """Validate form."""
+        username = self.ids.us1.text
+        user_pass = self.ids.us3.text
+        home = self.ids.us4.text
+        shell = self.ids.us5.text
+        # if self.active is True and self.ids.pr1.text
+        # is not self.ids.pr2.text:
+        # print(self.ids.pr1.focus)
+        # popup = InfoPopup()
+        # popup.set_info('Root passwords do not match')
+        # popup.open()
+        user = User()
+        user.save_user(username, user_pass, home, shell)
